89930aec98d8c8cda75f9fbab45e97fe8f45c6a5
YanaRyazanova <yanaryazanovs@gmaul.com>
2021-06-06 10:52:56 +0500

    Add api for comments

--- controllers/commentControl.js
+++ controllers/commentControl.js
@@ -1,8 +1,15 @@
-const commentModel = require('../models/comments.js');
+const CommentModel = require('../models/comments.js');
 
 class commentController {
     async addComment(req, res) {
         const { user, x, y, comment } = req.body;
+        const newComment = new CommentModel({
+            "user": user,
+            "x": x,
+            "y": y,
+            "comment": comment
+        });
+        await newComment.save();
         res.status(200);
     }
 }
